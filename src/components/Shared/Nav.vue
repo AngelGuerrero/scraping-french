2ac6fe7c06220fa33f7cbc5d15beebbe2ecbab1b
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2019-12-18 20:05:52 -0600

    Input text and reload list components

---
+++ src/components/Shared/Nav.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    #nav
+        router-link(to="/") Inicio
+        router-link(to="/about") Buscar
+        router-link(to="/input") Ingresar texto
+</template>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
